c410a3a2e1750d507a35d1c0434f2916ebc70c09
Raziel Sotken <zromik@mail.ru>
2023-10-11 21:32:03 +0400

    Реализовано вью для добавления аккаунта пользователя.

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class RegisterUserView(CreateView):
+    form_class = UserCreationForm
+    template_name = "authapp/register.html"
+    success_url = reverse_lazy("homeapp:index")
