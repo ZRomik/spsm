3315801c81fa34d2384e2439358946710f486f65
Raziel Sotken <zromik@mail.ru>
2023-10-11 21:31:20 +0400

    Обновлены тесты.

--- authapp/tests.py
+++ authapp/tests.py
@@ -16,7 +16,7 @@ class TestAuthApp(TestCase):
     def tearDownClass(cls):
         cls.simple_user.delete()
 
-    def test_user_login(self):
+    def test_login_user(self):
         self.client.logout()
         login_url = reverse_lazy("authapp:login")
         # проверка получения страницы аутентификации
@@ -53,7 +53,7 @@ class TestAuthApp(TestCase):
             f"Ошибка! Пользователь не авторизован!"
         )
 
-    def test_user_logout(self):
+    def test_logout_user(self):
         if not self.simple_user.is_authenticated:
             self.client.force_login(self.simple_user)
         logout_url = reverse_lazy("authapp:logout")
@@ -69,4 +69,59 @@ class TestAuthApp(TestCase):
             response.url,
             home_url,
             "Ошибка! Адрес редиректа после выхода из аккаунта не совпадает с адресом домашней страницы.."
+        )
+
+    def test_register_user(self):
+        register_url = reverse_lazy("authapp:register")
+        response = self.client.get(register_url)
+        self.assertEqual(
+            response.status_code,
+            200,
+            f"Ощибка! Не получена страница регистрации!"
+        )
+        data = {
+            "username": "test",
+            "password1": "test123_123",
+            "password2": "test123_123"
+        }
+        response = self.client.post(register_url, data=data)
+        self.assertEqual(
+            response.status_code,
+            302,
+            f"Ошибка! Нет редиректа!"
+        )
+        home_url = reverse_lazy("homeapp:index")
+        self.assertEqual(
+            response.url,
+            home_url,
+            "Ошибка! Адреса не равны!"
+        )
+        user = User.objects.get(username="test")
+        self.assertEqual(
+            type(user),
+            User,
+            "Ошибка! Типы данных не равны!"
+        )
+        user.delete()
+
+    def test_register_user_error(self):
+        register_url = reverse_lazy("authapp:register")
+        data = {
+            "username": "test",
+            "password1": "test123_123",
+            "password2": "test123_1233"
+        }
+        response = self.client.post(register_url, data=data)
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Получен неверный код ответа!"
+        )
+        print(response)
+        errors = response["error"]
+        print(errors)
+        self.assertContains(
+            response=response["errors"],
+            text="errors",
+            msg_prefix="Пропущена ошибка при регистрации!"
         )
