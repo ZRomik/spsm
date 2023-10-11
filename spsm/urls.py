35a85edf505b47fabf12547ea2bdc42f52b122fd
Raziel Sotken <zromik@mail.ru>
2023-10-11 13:47:08 +0400

    Настроен маршрут к приложению.

--- spsm/urls.py
+++ spsm/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("homeapp.urls"))
+    path("", include("homeapp.urls")),
+    path("auth/", include("authapp.urls"))
 ]
 
 if settings.DEBUG:
