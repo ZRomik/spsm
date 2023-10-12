da4b08d7572f081ad6b3bd794f33d5c830cf8bec
Raziel Sotken <zromik@mail.ru>
2023-10-12 13:34:30 +0400

    Добавлено разрешение, необходмое для вью.

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-@permission_required("add_user")
+@permission_required("add_profile")
 def create_user_profile_view(request: HttpRequest, id: int) -> HttpResponse:
     pass
