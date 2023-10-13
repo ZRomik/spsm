9f4d5e8e3d5d69822b54fc71307a237fc269916a
Raziel Sotken <zromik@mail.ru>
2023-10-13 10:18:41 +0400

    Настроен обработчик ошибки.

--- spsm/urls.py
+++ spsm/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import handler400, handler403, handler404, handler500
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
+handler404 = "homeapp.views.page_not_found_view"
 
 
 urlpatterns = [
