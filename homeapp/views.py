a8f492ac7fe23fe4e2f909f28e55a1f54f44d206
Raziel Sotken <zromik@mail.ru>
2023-10-13 10:18:18 +0400

    Добавлено вью для рендера страницы ошибки.

--- homeapp/views.py
+++ homeapp/views.py
@@ -4,3 +4,9 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "homeapp/home.html"
+
+def page_not_found_view(request, exception):
+    context = {
+        "path": request.path
+    }
+    return render(request, 'homeapp/404.html', context=context, status=404)
