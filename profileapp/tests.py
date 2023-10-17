ef40cd64fb6756781a952a96c0975f0372597ebc
Raziel Sotken <zromik@mail.ru>
2023-10-17 18:14:04 +0400

    Обновлены тесты.

--- profileapp/tests.py
+++ profileapp/tests.py
@@ -8,86 +8,57 @@ class TestProfileApp(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        super().setUpClass()
         cls.simple_user = User.objects.create_user(
             username="simple",
             password="simple"
         )
-        cls.advance_user = User.objects.create_user(
-            username="advance",
-            password="advance"
+        cls.advanced_user = User.objects.create_user(
+            username="advanced",
+            password="advanced"
         )
-        cls.advance_user.is_staff = True
-        add_profile_perm = Permission.objects.get(
+        add_perm = Permission.objects.get(
             codename="add_profile"
         )
-        if add_profile_perm:
-            cls.advance_user.user_permissions.add(add_profile_perm)
+        cls.advanced_user.user_permissions.add(add_perm)
 
     @classmethod
     def tearDownClass(cls):
-        super().tearDownClass()
-        cls.simple_user.delete()
-        cls.advance_user.delete()
+        cls.advanced_user.delete()
 
-    def test_create_profile_without_perm(self):
+    def test_create_user_profile_without_perm(self):
         self.client.force_login(self.simple_user)
         create_url = reverse_lazy("profileapp:create", kwargs={"id": self.simple_user.id})
+        response = self.client.get(create_url)
         response = self.client.post(create_url)
         self.assertEqual(
             response.status_code,
             302,
-            "Пользователь не имеет прав на создание профиля. Не выполнен редирект!"
+            "Неверный код ответа!"
         )
 
-    def test_create_profile_with_perm(self):
-        self.client.force_login(self.advance_user)
-        create_url = reverse_lazy("profileapp:create", kwargs={"id": self.advance_user.id})
-        response = self.client.post(create_url)
-        self.assertEqual(
-            response.status_code,
-            302,
-            "Нет редиректа!"
-        )
-        redirect_url = reverse_lazy("homeapp:index")
-        self.assertEqual(
-            response.url,
-            redirect_url,
-            "Адрес редиректа неверный!"
-        )
-        profile = Profile.objects.get(user_id=self.advance_user.id)
-        self.assertFalse(
-            profile is None,
-            "Не удалось найти созданный профиль!"
-        )
-        profile.delete()
-
-    def test_create_profile_with_get_method(self):
-        self.client.force_login(self.advance_user)
-        create_url = reverse_lazy("profileapp:create", kwargs={"id": self.advance_user.id})
+    def test_create_user_profile_with_perm(self):
+        self.client.logout()
+        self.client.force_login(self.advanced_user)
+        create_url = reverse_lazy("profileapp:create", kwargs={"id": self.advanced_user.id})
         response = self.client.get(create_url)
         self.assertEqual(
             response.status_code,
             405,
-            "Выполнен запрос методом 'GET'. Получен неверный код ответа!"
+            "Неверный код ответа GET!"
         )
-
-    def test_create_profile_without_id(self):
-        self.client.force_login(self.advance_user)
-        create_url = '/profile/create/'
         response = self.client.post(create_url)
-        self.assertEqual(
-            response.status_code,
-            404,
-            "Не указан id юзера. Неверный код ответа!"
+        index_url = reverse_lazy("homeapp:index")
+        self.assertRedirects(
+            response,
+            index_url,
+            msg_prefix="Неверный редирект!"
         )
-
-    def test_create_profile_with_wrong_user_id(self):
-        self.client.force_login(self.advance_user)
-        create_url = reverse_lazy("profileapp:create", kwargs={"id": 26})
-        response = self.client.post(create_url)
-        self.assertEqual(
-            response.status_code,
-            404,
-            "Указан неверный ид юзера. Неверный код ответа!"
+        profile = Profile.objects.get(
+            user=self.advanced_user
         )
+        self.assertEqual(
+            type(profile),
+            Profile,
+            "Неверный тип объекта!"
+        )
+        profile.delete()
