5e8056c8b0648e8abb3cba3b4f408bc79ed3e814
Raziel Sotken <zromik@mail.ru>
2023-10-22 21:04:06 +0400

    Настроены маршруты к страница аутентификации и выхода из аккаунта.

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, reverse_lazy
 
 app_name = "authapp"
 
 urlpatterns = [
+    path(
+        "login/",
+        LoginView.as_view(template_name="authapp/login.html", redirect_authenticated_user=True),
+        name="login"
+    ),
+    path(
+        "logout/",
+        LogoutView.as_view(next_page=reverse_lazy("homeapp:index")),
+        name="logout"
+    )
 ]
