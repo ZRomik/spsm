f8c5fb6b3a381a1213a69d9f18809da2d0e7e13a
Raziel Sotken <zromik@mail.ru>
2023-10-22 16:58:04 +0400

    Добавлены маршруты к вью.

---
+++ profileapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import (
+    ProfileDetailView,
+    create_new_profile_view,
+)
+
+app_name = "profileapp"
+
+urlpatterns = [
+    path("detail/<int:pk>/", ProfileDetailView.as_view(), name="detail"),
+    path("create/<int:id>/", create_new_profile_view, name="create"),
+]
