9ec93bb6823558801a8c12bd7b0c152c04c7c38e
Raziel Sotken <zromik@mail.ru>
2023-10-25 09:14:21 +0400

    Настроен маршрут к странице редактирования профиля.

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     ProfileDetailView,
     create_new_profile_view,
+    UpdateProfileView,
 )
 
 app_name = "profileapp"
@@ -9,4 +10,5 @@ app_name = "profileapp"
 urlpatterns = [
     path("detail/<int:pk>/", ProfileDetailView.as_view(), name="detail"),
     path("create/<int:id>/", create_new_profile_view, name="create"),
+    path("update/<int:pk>/", UpdateProfileView.as_view(), name="update")
 ]
