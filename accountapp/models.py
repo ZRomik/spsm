a18b4bbc2ac0001bf49b499ea35356e0ace1a85b
Raziel Sotken <zromik@mail.ru>
2023-10-29 12:49:06 +0400

    Реализована модель профиля.

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+def get_profile_avatar_filepath(instance: "Profile", filename: str) -> str:
+    if filename:
+        return f"accounts/profile{instance.pk}/{filename}"
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name="profile")
+    lastname = models.CharField(max_length=128, null=True)
+    firstname = models.CharField(max_length=128, null=True)
+    middlename = models.CharField(max_length=128, null=True)
+    nmn = models.BooleanField(default=False, null=True)
+    ext_phone = models.CharField(max_length=128, null=True)
+    ser_phone = models.CharField(max_length=128, null=True)
+    pri_phone = models.CharField(max_length=128, null=True)
+    avatar = models.ImageField(upload_to=get_profile_avatar_filepath, null=True)
+    email = models.CharField(max_length=128, null=True)
