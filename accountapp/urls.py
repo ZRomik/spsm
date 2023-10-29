cb4a281d9695fc389ed0f63b614f58345640b85f
Raziel Sotken <zromik@mail.ru>
2023-10-29 18:23:05 +0400

    Настроен маршрут к странице профиля.

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, reverse_lazy
 from .views import (
     RegisterUserView,
+    ProfileDetailsView
 )
 
 app_name = "accountapp"
@@ -12,5 +13,6 @@ urlpatterns = [
         LoginView.as_view(template_name="accountapp/login.html", redirect_authenticated_user=True),
         name="login"),
     path("logout/", LogoutView.as_view(next_page=reverse_lazy("homeapp:index")), name="logout"),
-    path("register/", RegisterUserView.as_view(), name="register")
+    path("register/", RegisterUserView.as_view(), name="register"),
+    path("profiles/details/<int:pk>", ProfileDetailsView.as_view(), name="details")
 ]
