9482b15b9f2b0b1230845e1264f0d58d88407d5d
Raziel Sotken <zromik@mail.ru>
2023-10-29 12:47:51 +0400

    Реализовано вью для регистрации аккаунта и создания профиля.

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .models import Profile
 
-# Create your views here.
+
+class RegisterUserView(PermissionRequiredMixin, CreateView):
+    permission_required = "add_user"
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, "accountapp/register.html")
+
+    def post(self, request, *args, **kwargs):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            profile = Profile.objects.create(
+                user=user
+            )
+            url = reverse_lazy("homeapp:index")
+            return redirect(url)
+        else:
+            return render(request, "accountapp/register.html", context={"form": form})
