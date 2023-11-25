ef32a6c674f6107cd873857ccc4662542f99e2de
Raziel Sotken <zromik@mail.ru>
2023-11-25 22:34:14 +0400

    Реализовано представление для регистрации аккаунта.

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import View
+from .services import create_user
 
-# Create your views here.
+
+class RegisterUserView(PermissionRequiredMixin, View):
+    permission_required = "auth.add_user"
+
+    def get(self, request):
+        return render(request, "authapp/register.html")
+
+    def post(self, request, *args, **kwargs):
+        created, user, form = create_user(request=request)
+        if not created:
+            context = {
+                "form": form
+            }
+            return render(request, "authapp/register.html", context=context)
+        else:
+            url = reverse_lazy("homeapp:index")
+            return redirect(url)
