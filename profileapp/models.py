86d25320ceadc0aa872910d4c0e669c6753cd3e1
Raziel Sotken <zromik@mail.ru>
2023-12-01 10:42:46 +0400

    Реализована модель аватара пользователя.

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+def get_avatar_file_path(instance: "Avatar", filename: str) -> str:
+    if filename:
+        return f"avatars/{instance.pk}/{filename}"
+
+class Avatar(models.Model):
+    """
+    Модель описывает аватар пользователя
+    """
+    avatar = models.ImageField(upload_to=get_avatar_file_path, null=True)
+
+    class Meta:
+        db_table = "avatars"
