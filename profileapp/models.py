25d0095e0e1cd0f13e0e5f0e1b829eae8b9c5eb2
Raziel Sotken <zromik@mail.ru>
2023-12-02 14:47:40 +0400

    Добавлена модель профиля пользователя.

--- profileapp/models.py
+++ profileapp/models.py
@@ -33,3 +33,31 @@ class Email(models.Model):
 
     class Meta:
         db_table = "emails"
+
+class Profile(models.Model):
+    """
+    Модель описывает профиль пользователя
+    """
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    lastname = models.CharField(max_length=128, null=True)
+    firstname = models.CharField(max_length=128, null=True)
+    middlename = models.CharField(max_length=128, null=True)
+    nmn = models.BooleanField(default=False)
+    work_mail = models.EmailField(max_length=128, null=True)
+    ext_phone = models.CharField(max_length=128, null=True)
+    work_phone = models.CharField(max_length=128, null=True)
+    self_phone = models.CharField(max_length=128, null=True)
+
+    @property
+    def fio(self):
+        text = "Нет данных"
+        if self.lastname:
+            text = self.lastname
+            if self.firstname:
+                text.join([" ", self.firstname[:1], "."])
+                if self.middlename:
+                    text.join([" ", self.middlename[:1], "."])
+        return text
+
+    class Meta:
+        db_table = "profiles"
