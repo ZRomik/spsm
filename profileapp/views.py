9c396e83433faad561208db67ae2664cdb8a1c07
Raziel Sotken <zromik@mail.ru>
2023-12-03 13:34:36 +0400

    Реализовано представление для просмотра профиля.

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import DetailView
+from .models import Profile
+from .services import get_profile
 
-# Create your views here.
+class ProfileDetailView(LoginRequiredMixin, DetailView):
+    permission_required = "profileapp.add_profile"
+    queryset = Profile.objects.all()
+    context_object_name = "profile"
+
+    def get(self, request, pk):
+        try:
+            user = User.objects.get(pk=pk)
+            profile = get_profile(user)
+            if profile:
+                context = {
+                    "profile": profile
+                }
+                return render(request, "profileapp/profile_detail.html", context=context)
+        except Exception as e:
+            return HttpRequest(status_code=404)
