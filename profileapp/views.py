9861d4ee13d6ac53d90ba3dc6b1a4aff0000e0bd
Raziel Sotken <zromik@mail.ru>
2023-12-03 17:47:13 +0400

    Обновлено преджставление.

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseNotFound, HttpResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import DetailView
@@ -9,17 +9,15 @@ from .services import get_profile
 
 class ProfileDetailView(LoginRequiredMixin, DetailView):
     permission_required = "profileapp.add_profile"
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.all().prefetch_related("avatar").prefetch_related("phones").prefetch_related("emails")
     context_object_name = "profile"
 
     def get(self, request, pk):
-        try:
-            user = User.objects.get(pk=pk)
-            profile = get_profile(user)
+            profile = get_profile(user_id=pk)
             if profile:
                 context = {
                     "profile": profile
                 }
                 return render(request, "profileapp/profile_detail.html", context=context)
-        except Exception as e:
-            return HttpRequest(status_code=404)
+            else:
+                return HttpResponse(status=404)
