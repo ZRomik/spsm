0ceb17b46579c4167669a83b0d2a6153b32562e1
Raziel Sotken <zromik@mail.ru>
2023-12-03 17:46:40 +0400

    Оптимизирован код функции get_profile

--- profileapp/services.py
+++ profileapp/services.py
@@ -1,6 +1,11 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+
 from .models import Profile
 
-def get_profile(user) -> Profile:
-    return Profile.objects.get_or_create(
-        user=user
-    )[0]
+def get_profile(user_id: int) -> Profile:
+    try:
+        user = User.objects.get(pk=user_id)
+        return Profile.objects.get_or_create(user=user)[0]
+    except ObjectDoesNotExist as e:
+        return None
