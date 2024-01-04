d37def799f304bf60b28d176d311f06204e425f1
Raziel Sotken <zromik@mail.ru>
2024-01-04 18:07:09 +0400

    Описана модель профиля пользователя.

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    """
+    Модель описывает профиль пользователя
+    """
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) # ссылка на аккаунт пользователя
+    firstname = models.CharField(max_length=128, verbose_name="Имя") # реальное имя
+    lastname = models.CharField(max_length=128, verbose_name="Фамилия") # фамилия
+    middlename = models.CharField(max_length=128, verbose_name="Отчество", null=True, blank=True) # отчество (опционально)
+    nmn = models.BooleanField(default=False, verbose_name="Нет отчества") # флаг, что у пользователя нет отчества
+    extphone = models.CharField(max_length=128, null=True, blank=True, verbose_name="Вн. тел.") # внут. тел.
+    workphone = models.CharField(max_length=128, null=True, blank=True, verbose_name="Сл. тел.") # служ. тел.
+    selfphone = models.CharField(max_length=128, null=True, blank=True, verbose_name="Лич. тел.") # лич. тел.
+    workmail = models.EmailField(max_length=128, null=True, blank=True, verbose_name="Раб. почта") # раб. эл. почта
+
+    class Meta:
+        db_table = "profiles"
