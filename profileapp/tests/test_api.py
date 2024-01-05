96fe3d5bb745e69978e658f19570dc48b97d8724
Raziel Sotken <zromik@mail.ru>
2024-01-05 17:00:59 +0400

    Добавлено тестирование функции get_avatar.

--- profileapp/tests/test_api.py
+++ profileapp/tests/test_api.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
-from ..api import get_user_profile
+from ..api import get_user_profile, get_avatar
 from ..models import Profile, Avatar
 """
 Тестирование вспомогательных функций приложения
@@ -45,4 +45,50 @@ class GetUserProfileTestCase(TestCase):
         self.assertIsNotNone(
             profile,
             "Не получен объект модели профиля!"
+        )
+
+class GetAvatarTestCase(TestCase):
+    """
+    Тестирование функции get_avatar
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(
+            username="user",
+            password="user"
+        )
+        created, cls.profile = get_user_profile(id = cls.user.id)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls.profile.delete()
+        cls.user.delete()
+        for avatar in Avatar.objects.all():
+            avatar.delete()
+        for profile in Profile.objects.all():
+            profile.delete()
+
+    def test_get_avatar_error(self):
+        created, avatar = get_avatar(profile=None)
+        self.assertFalse(
+            created,
+            "В ответе получена истина!"
+        )
+        self.assertIsNone(
+            avatar,
+            "Получен объект модели аватар!"
+        )
+
+    def test_get_avatar_ok(self):
+        created, avatar = get_avatar(profile=self.profile)
+        self.assertTrue(
+            created,
+            "В ответе получена ложь!"
+        )
+        self.assertIsNotNone(
+            avatar,
+            "Не получен объект модели аватар!"
         )
