dbe02a57cf3d343d9e4b032d0d8ee3721feb5702
Raziel Sotken <zromik@mail.ru>
2024-02-12 15:53:45 +0400

    Реализовано представление для отображения страницы с информацией о проекте.

--- aboutapp/views.py
+++ aboutapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class AboutPageView(TemplateView):
+    template_name = "aboutapp/about-page.html"
