e66e02c8c13a40d20d1aaef0887598a504064b65
Raziel Sotken <zromik@mail.ru>
2024-02-16 14:35:23 +0400

    Настроен маршрут к странице авторизации.

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
+from .views import register_user_account
 
 app_name = "auths"
 urlpatterns = [
-
+    path(
+        "login/",
+        LoginView.as_view(
+            template_name="authapp/login.html"
+        ),
+        name="login"
+    )
 ]
