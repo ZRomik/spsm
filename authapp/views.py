467e34509e9d40a0e3579a140b1ccbe18ccd847d
Raziel Sotken <zromik@mail.ru>
2024-02-16 14:34:54 +0400

    Реализовано представление для регистрации аккаунта.

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from .forms import SPSMUserCreationForm
+from .api import get_user_account
+from django.contrib.auth.decorators import permission_required
 
-# Create your views here.
+
+@permission_required("auth.add_user")
+def register_user_account(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        form = SPSMUserCreationForm()
+        return render(request, "authapp/register-account.html", context={"form": form})
+    elif request.method == "POST":
+        created, user, form = get_user_account(request.POST)
+        if created:
+            return redirect("/")
+        else:
+            return HttpResponse(status=404)
