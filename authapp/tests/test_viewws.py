7da05cd8097fdc010c9f442dea81053e7e94cc51
Raziel Sotken <zromik@mail.ru>
2024-02-17 15:33:15 +0400

    Реализовано тестирование представления для выхода из аккаунта.

--- authapp/tests/test_viewws.py
+++ authapp/tests/test_viewws.py
@@ -38,3 +38,33 @@ class LoginViewTestCase(TestCase):
             "/",
             "Неверный адрес редиректа!"
         )
+
+class LogoutViewTestCase(TestCase):
+    """
+    Тестирование представления для выхода из аккаунта.
+    """
+    def setUp(self) -> None:
+        super().setUp()
+        self.test_user = User.objects.create_user(
+            username="test",
+            password="test"
+        )
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        self.test_user.delete()
+
+    def test_logout_user_ok(self):
+        self.client.force_login(self.test_user)
+        url = reverse_lazy("auths:logout")
+        response = self.client.get(url)
+        self.assertEqual(
+            response.status_code,
+            302,
+            "Неверный код ответа!"
+        )
+        self.assertEqual(
+            response.url,
+            "/",
+            "Неверный адрес редиректа!"
+        )
