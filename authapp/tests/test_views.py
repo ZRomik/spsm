49a9d60896c4b953a166046a0446bf0beb981c1f
Raziel Sotken <zromik@mail.ru>
2024-02-19 21:46:23 +0400

    Переименован файл тестирования представлений.

--- authapp/tests/test_views.py
+++ authapp/tests/test_views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group, Permission
 from django.test import TestCase
 from django.urls import reverse_lazy
 
@@ -85,4 +85,85 @@ class LogoutViewTestCase(TestCase):
             response.url,
             "/",
             "Неверный адрес редиректа!"
+        )
+
+class RegisterUserAccountTestCase(TestCase):
+    """
+    Тестирование представления для регистрации аккаунта.
+    """
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.creator = User.objects.create_user(
+            username="creator",
+            password="creator"
+        )
+        cls.group = Group.objects.create(name="test-group"
+                                          )
+        add_user_perm = Permission.objects.get(
+            codename="add_user"
+        )
+        cls.group.permissions.add(add_user_perm)
+        cls.group.save()
+        cls.creator.groups.add(cls.group)
+        cls.creator.save()
+        cls.register_data_ok = {
+            "username": "simpleuser",
+            "password1": "simplepass123",
+            "password2": "simplepass123",
+            "email": "simple@mail.net"
+        }
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls.creator.delete()
+        cls.group.delete()
+        try:
+            user = User.objects.get(
+                username="simpleuser"
+            )
+            if user:
+                user.delete()
+        except:
+            pass
+
+    def test_register_user_view_ok(self):
+        self.client.force_login(self.creator)
+        url = reverse_lazy("auth:register")
+        response = self.client.post(url, self.register_data_ok)
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Неверный код ответа!"
+        )
+        self.assertContains(
+            response=response,
+            text="Аккаунт создан.",
+            msg_prefix="Нет сообщения о создании аккаунта!"
+        )
+        try:
+            simple_user = User.objects.get(
+                username="simpleuser"
+            )
+        except:
+            pass
+        self.assertIsNotNone(
+            simple_user,
+            "Аккаунт не найден!"
+        )
+        simple_user.delete()
+
+    def test_register_user_view_error_405(self):
+        self.client.force_login(self.creator)
+        url = reverse_lazy("auth:register")
+        response = self.client.put(
+            path=url,
+            extra=self.register_data_ok
+        )
+        self.assertEqual(
+            response.status_code,
+            405,
+            "Неверный код ответа!"
+
         )
