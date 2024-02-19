6236bcd137b70120715105efeb559679e0b1b880
Raziel Sotken <zromik@mail.ru>
2024-02-19 21:44:53 +0400

    Реализовано представление для создания аккаунта.

--- authapp/views.py
+++ authapp/views.py
@@ -8,12 +8,16 @@ from django.contrib.auth.decorators import permission_required
 
 @permission_required("auth.add_user")
 def register_user_account(request: HttpRequest) -> HttpResponse:
-    if request.method == 'GET':
-        form = SPSMUserCreationForm()
-        return render(request, "authapp/register-account.html", context={"form": form})
+    if request.method == "GET":
+        return render(request, "authapp/register-account.html")
     elif request.method == "POST":
         created, user, form = get_user_account(request.POST)
         if created:
-            return redirect("/")
-        else:
-            return HttpResponse(status=404)
+            return HttpResponse("Аккаунт создан.", status=200)
+        elif not created and form:
+            context = {
+                "form": form
+            }
+            return render(request, "authapp/register-account.html", context=context)
+    else:
+        return HttpResponse(status=405)
