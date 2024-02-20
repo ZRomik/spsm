e50d79c3e1bcad8fd5979cda4bd226f87564c6a6
Raziel Sotken <zromik@mail.ru>
2024-02-20 20:18:16 +0400

    Исправлено тестирование.

--- authapp/tests/test_api.py
+++ authapp/tests/test_api.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django.test import RequestFactory, TestCase
 from ..api import get_or_create_user_account
 import logging
@@ -13,20 +14,16 @@ class APITestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
-        cls.register_data_and_email = {
-            "POST": {
+        cls.register_data_ok = {
                 "username": "test",
                 "password1": "test123123",
                 "password2": "test123123",
                 "email": "mail@testmail.com"
-            }
         }
-        cls.register_data_no_email = {
-            "POST": {
+        cls.register_data_error = {
                 "username": "test",
                 "password1": "test123123",
                 "password2": "test123123",
-            }
         }
 
     def tearDown(self) -> None:
@@ -40,11 +37,14 @@ class APITestCase(TestCase):
         Тестирование создания учетной записи пользователя.
         """
         factory = RequestFactory()
-        request = factory.post("")
-        request.POST = self.register_data_no_email
+        request = factory.post(path="", data=self.register_data_ok)
+        # request.POST = self.register_data_ok
         created, user, form = get_or_create_user_account(request)
         if form:
-            print(f"Ошибка в данных: {form.errors}")
+            for field in form:
+                for error in field.errors:
+                    print(f"{field.label}: {error}")
+            # print(f"Ошибка в данных: {form.errors}")
         self.assertTrue(
             created,
             "Аккаунт не создан!"
@@ -57,11 +57,11 @@ class APITestCase(TestCase):
         factory = RequestFactory()
         request = factory.post("")
         request.user = AnonymousUser
-        request.POST = self.register_data_and_email
+        request.POST = self.register_data_error
         created, user, form = get_or_create_user_account(request)
         self.assertFalse(
             created,
-            "Аккаунто создан!"
+            "Аккаунт создан!"
         )
         self.assertIsNotNone(
             form.errors,
@@ -71,3 +71,10 @@ class APITestCase(TestCase):
             'email' in form.errors.keys(),
             "Нет адреса эл. почты!"
         )
+        with self.assertRaises(
+            expected_exception=ObjectDoesNotExist,
+            msg="Аккаунт найден!"
+        ):
+            user = User.objects.get(
+                username="test"
+            )
