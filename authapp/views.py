d80af1692a360c1e236bf76f78fbae9b776a937e
Raziel Sotken <zromik@mail.ru>
2024-02-20 13:45:46 +0400

    В представление register_user_account добавлено логирование.

--- authapp/views.py
+++ authapp/views.py
@@ -4,10 +4,16 @@ from django.shortcuts import render, redirect
 from .forms import SPSMUserCreationForm
 from .api import get_user_account
 from django.contrib.auth.decorators import permission_required
+import logging
 
+logger = logging.getLogger(__name__)
 
 @permission_required("auth.add_user")
 def register_user_account(request: HttpRequest) -> HttpResponse:
+    logger.info(
+        "Вызов register_user_account"
+    )
+
     if request.method == "GET":
         return render(request, "authapp/register-account.html")
     elif request.method == "POST":
@@ -15,9 +21,23 @@ def register_user_account(request: HttpRequest) -> HttpResponse:
         if created:
             return HttpResponse("Аккаунт создан.", status=200)
         elif not created and form:
+            logger.error(
+                msg="Аккаунт не создан!",
+                extra={
+                    "username": request.user,
+                    "funcame": __name__,
+                }
+            )
             context = {
                 "form": form
             }
             return render(request, "authapp/register-account.html", context=context)
     else:
+        logger.error(
+            msg="Тип запроса не поддерживается",
+            extra={
+                "username": request.user,
+                "funcname": __name__
+            }
+        )
         return HttpResponse(status=405)
