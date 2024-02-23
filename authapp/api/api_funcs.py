e72e1c60f65fc16ee8581d5e4f85acc553d5d6aa
Raziel Sotken <zromik@mail.ru>
2024-02-23 22:04:26 +0400

    Рефакторинг кода функции.

--- authapp/api/api_funcs.py
+++ authapp/api/api_funcs.py
@@ -8,17 +8,17 @@ def get_or_create_user_account(request):
         username = request.user.username
     except:
         username = "Незарегистрированный пользователь"
-
-    logging.info(
-        f"Обращение к АПИ.",
-        extra={
-            "username": username
-        }
-    )
     form = SPSMUserCreationForm(request.POST)
     user = None
     if form.is_valid():
         user = form.save()
         user.email = form.cleaned_data["email"]
         user.save()
+    else:
+        logger.warning(
+            "Не удалось создать аккаунт. Ошибка в  полученных данных.",
+            extra={
+                "username": username
+            }
+        )
     return  user is not None, user, form if not user else None
