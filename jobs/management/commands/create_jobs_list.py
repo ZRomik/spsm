751778ad0b32c9746083da46888a6ccc3e2cc1a8
Raziel Sotken <zromik@mail.ru>
2024-03-20 14:52:04 +0400

    Рефакторинг кода команды для настройки списка должностей.

--- jobs/management/commands/create_jobs_list.py
+++ jobs/management/commands/create_jobs_list.py
@@ -5,8 +5,12 @@ from jobs.models import  Job
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write(
-            self.style.WARNING("Создание списка рабочих должностей...")
+            self.style.WARNING("Создание списка должностей...")
         )
+        # Первая запись в таблице, будет значением по умолчанию для профиля.
+        Job.objects.get_or_create(
+            title="Не указана"
+        )[0].save()
         Job.objects.get_or_create(
             title="Программист"
         )[0].save()
