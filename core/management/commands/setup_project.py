82dffada3f02db215685b7758725f260f5d50f6b
Raziel Sotken <zromik@mail.ru>
2024-03-20 14:54:42 +0400

    Удален неиспользуемый импорт.

--- core/management/commands/setup_project.py
+++ core/management/commands/setup_project.py
@@ -1,5 +1,3 @@
-from django.contrib.auth.models import Group, Permission
-from django.contrib.contenttypes.models import ContentType
 from django.core.management import BaseCommand
 from django.core.management import call_command
 
