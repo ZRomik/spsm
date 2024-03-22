5fdbbf83b72eeb25eab08968b59a9b2df1ad9e8b
Raziel Sotken <zromik@mail.ru>
2024-03-22 11:21:59 +0400

    Реализована модель профиля пользователя.

--- profileapp/models/model_profile.py
+++ profileapp/models/model_profile.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from jobs.models import Job
 from departments.models import Department
@@ -7,6 +8,8 @@ class Profile(models.Model):
     Модель профиля пользователя.
     fields:
     user OneToOneField: ссылка на аккаунт пользователя.
+    job :model:Job : ссылка на должность.
+    dept :model:Department : ссылка на подразделение.
     lastname CharField: фамилия.
     firstname CharField: имя
     middlename CharField: отчество
@@ -14,3 +17,30 @@ class Profile(models.Model):
     phone CharField: доб. ном. тел.
     mail EmailField: корп. эл. почта.
     """
+
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, related_name="profile")
+    job = models.OneToOneField(Job, on_delete=models.PROTECT, related_name="job_title", default=1)
+    dept = models.OneToOneField(Department, on_delete=models.PROTECT, default=1, related_name="department")
+    lastname = models.CharField(max_length=128)
+    firstname = models.CharField(max_length=128)
+    middlename = models.CharField(max_length=128, blank=True)
+    nmn = models.BooleanField(default=False)
+    phone = models.CharField(max_length=100)
+    mail = models.EmailField(max_length=100)
+
+    @property
+    def fio(self):
+        """
+        Возвращает ФИО сотрудника
+        """
+        result = "Нет данных"
+        if self.lastname:
+            result = self.lastname.capitalize()
+            if self.firstname:
+                result = "".join([result, " ", self.firstname[:1].upper(), "."])
+                if self.middlename and not self.nmn:
+                    result = "".join([result, " ", self.middlename[:1].upper(), "."])
+            return result
+
+    class Meta:
+        db_table = "profiles"
