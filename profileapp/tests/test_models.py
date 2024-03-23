afcd8e7b7085e1802aa18b5c6fc6692c90408835
Raziel Sotken <zromik@mail.ru>
2024-03-23 18:08:35 +0400

    Реализовано тестирование модели Profile.

--- profileapp/tests/test_models.py
+++ profileapp/tests/test_models.py
@@ -1,36 +1,48 @@
-from django.contrib.auth.models import User
-from django.test import TransactionTestCase
 from jobs.models import Job
 from departments.models import Department
+from django.test import TransactionTestCase
+from django.contrib.auth.models import User
 from ..models import Profile
+from django.db.utils import IntegrityError
+import logging
+
+logger = logging.getLogger(__name__)
 
 class ModelProfileTestCase(TransactionTestCase):
     """
-    Тестирование модели профля пользователя.
+    Тестирование модели :model:Profile
     """
 
-    def setUp(self) -> None:
-        super().setUp()
-        Job.objects.get_or_create(
-            title="job"
-        )
-        Department.objects.get_or_create(
-            name="dept"
-        )
-        self.test_user = User.objects.create_user(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.test_user = User.objects.create_user(
             username="test",
             password="test"
         )
+        Job.objects.create(
+            title="job"
+        )
+        Department.objects.create(
+            name="dept"
+        )
 
-    def tearDown(self) -> None:
-        super().tearDown()
-        self.test_user.delete()
+    @classmethod
+    def tearDownClass(cls) -> None:
+        super().tearDownClass()
+        # удаляем все профили
+        if Profile.objects.count() > 0:
+            for profile in Profile.objects.all():
+                profile.delete()
+        # удаляем все должности
         for job in Job.objects.all():
             job.delete()
+        # удаляем все отделы
         for dept in Department.objects.all():
             dept.delete()
+        cls.test_user.delete()
 
-    def test_create_profile_ok(self):
+    def test_create_profile(self):
         profile = Profile.objects.create(
             user_id=self.test_user.pk
         )
@@ -38,5 +50,21 @@ class ModelProfileTestCase(TransactionTestCase):
             profile,
             "Профиль не создан!"
         )
-        if profile:
-            profile.delete()
+        self.assertEqual(
+            profile.job,
+            Job.objects.get(pk=1)
+        )
+        self.assertEqual(
+            profile.dept,
+            Department.objects.get(pk=1)
+        )
+        profile.delete()
+
+    def test_create_profile_integrity_error(self):
+        with self.assertRaises(
+            expected_exception=IntegrityError,
+            msg="Исключение не возбуждено!"
+        ):
+            profile = Profile.objects.create(
+                user_id=self.test_user.pk
+            )
