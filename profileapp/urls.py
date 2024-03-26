c688bd8aa7ae3c693f10eb56a94adca36a51a0cc
Raziel Sotken <zromik@mail.ru>
2024-03-26 14:59:33 +0400

    Настроен маршрут к странице профиля.

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import (
+    view_user_profile,
+)
 
 app_name = "profileapp"
 urlpatterns = [
-
+    path("profile/<int:pk>/", view_user_profile, name="profile"),
 ]
