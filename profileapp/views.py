4df0a5d369e08e541e47af4af534ac7355e733c3
Raziel Sotken <zromik@mail.ru>
2024-03-26 14:59:13 +0400

    Реализовано представление для визуализации страницы профиля.

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from .api import get_or_create_profile
+from .models import Profile
 
-# Create your views here.
+@login_required
+def view_user_profile(request: HttpRequest, pk: int) -> HttpResponse:
+    """
+    Представление для визуализацми профиля пользователя.
+    """
+    if request.method == "GET":
+        created, profile, error = get_or_create_profile(
+            user_pk=pk
+        )
+        if created:
+            context = {
+                "profile": profile
+            }
+            return render(request, "profileapp/profile-details.html", context=context)
+        else:
+            errors = list(error)
+            context = {
+                "errors": errors
+            }
+            return HttpResponse(status=404)
+    else:
+        return HttpResponse(status=405)
