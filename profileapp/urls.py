c35c94d549fbbe1bfb053a13035603c635167b53
Raziel Sotken <zromik@mail.ru>
2024-03-29 10:19:06 +0400

    Настроен маршрут к странице для смены аватара.

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
     view_user_profile,
+    change_avatar_view,
 )
 
 app_name = "profileapp"
 urlpatterns = [
     path("profile/<int:pk>/", view_user_profile, name="profile"),
+    path("avatar/change", change_avatar_view, name="change-avatar"),
 ]
