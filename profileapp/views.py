32859c03146aeb639aeee27258cec2becdddc912
Raziel Sotken <zromik@mail.ru>
2024-04-01 15:51:02 +0400

    Обновлен дизайн страницы для редактирования профиля.

--- profileapp/views.py
+++ profileapp/views.py
@@ -19,10 +19,6 @@ def view_user_profile(request: HttpRequest, pk: int) -> HttpResponse:
             }
             return render(request, "profileapp/profile-details.html", context=context)
         else:
-            errors = list(error)
-            context = {
-                "errors": errors
-            }
             return HttpResponse(status=404)
     else:
         return HttpResponse(status=405)
